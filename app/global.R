455f9996ef427ab933b5e5138369e9b2f2c1c7bb
Peter Möller <peter.moller@regiondalarna.se>
2026-03-02 16:58:10 +0100

    Flyttat över datainläsning till enbart vår databas. Döpt om kolumner till bara gemener och ändrat koden utifrån det

--- app/global.R
+++ app/global.R
@@ -8,20 +8,41 @@ library(writexl)
 library(ggtext)
 library(lubridate)
 
-source("R/mini_func_gis.R")
+source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_GIS.R", encoding = "utf-8", echo = FALSE)
 
 options(dplyr.summarise.inform = FALSE)
 
 # ---- 1. Läs in data ----
-brottsdata <- readRDS("data/brott_polisen.rds") %>% 
-  select(Kommunkod, Kommunnamn, desokod, regsonamn, Brottskod,  
-         `Inskr år`, `Inskr årmånad`, Inskrivningsdatum, antal_brott) %>% 
-  mutate(across(starts_with("Inskr "), as.character))
-brott_niva_nyckel <- readRDS("data/brott_niva_nyckel.rds")
-kommun_sf <- readRDS("data/kommun_sf.rds")
-deso_sf <- readRDS("data/deso_sf.rds")
-bef_df <- readRDS("data/bef_deso.rds")
-bra_kommunindikatorer <- readRDS("data/bra_kommunindikatorer.rds") %>% 
+
+# läs in från vår postgres-databas som heter oppna_data
+brottsdata <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("polisen", "brott")) %>%
+  select(kommunkod, kommunnamn, desokod, regsonamn, brottskod,
+         `inskr år`, `inskr årmånad`, inskrivningsdatum, antal_brott) %>%
+  mutate(across(starts_with("inskr "), as.character)) %>%
+  collect()
+
+# läs in från vår postgres-databas som heter oppna_data
+brott_niva_nyckel <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("polisen", "brott_niva_nyckel")) %>%
+  collect()
+
+# läs in från vår geodatabas i postgis/postgres
+kommun_sf <- hamta_karta("kommuner", regionkoder = "20") %>% 
+  rename(kommunkod = knkod, kommunnamn = knnamn) %>%
+  st_transform(crs = 4326)
+
+# läs in från vår geodatabas i postgis/postgres
+deso_sf <- hamta_karta("deso", regionkoder = "20") %>% 
+  rename(kommunkod = kommunkod, kommunnamn = kommunnamn) %>%
+  st_transform(crs = 4326)
+
+# läs in från vår postgres-databas som heter oppna_data - där vi skapat denna särskilt för att vi imputerar år som inte finns 
+# hos scb på deso och befolking men som finns i polisens data
+bef_df <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("brottsforebyggande", "bef_deso_med_imputering")) %>%
+  collect()
+
+# läs in från vår postgres-databas som heter oppna_data
+bra_kommunindikatorer <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("bra", "kommunindikatorer")) %>%
+  collect() %>% 
   filter(variabel != "Antal svarande i NTU.")
 
 # färgvektor
