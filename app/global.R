a40d073061a3227a525c21246c2111bf39052603
Peter Möller <peter.moller@regiondalarna.se>
2026-03-02 17:59:11 +0100

    Korrigerat för att ta bort func_GIS.R ur brott och ersatt med func_shinyappar.R som är anpassad för Shinyappar.

--- app/global.R
+++ app/global.R
@@ -8,41 +8,55 @@ library(writexl)
 library(ggtext)
 library(lubridate)
 
-source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_GIS.R", encoding = "utf-8", echo = FALSE)
+source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_shinyappar.R", encoding = "utf-8", echo = FALSE)
 
 options(dplyr.summarise.inform = FALSE)
 
 # ---- 1. Läs in data ----
 
 # läs in från vår postgres-databas som heter oppna_data
-brottsdata <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("polisen", "brott")) %>%
+brottsdata <- tbl(shiny_uppkoppling_las("oppna_data"), dbplyr::in_schema("polisen", "brott")) %>%
   select(kommunkod, kommunnamn, desokod, regsonamn, brottskod,
          `inskr år`, `inskr årmånad`, inskrivningsdatum, antal_brott) %>%
   mutate(across(starts_with("inskr "), as.character)) %>%
   collect()
 
 # läs in från vår postgres-databas som heter oppna_data
-brott_niva_nyckel <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("polisen", "brott_niva_nyckel")) %>%
+brott_niva_nyckel <- tbl(shiny_uppkoppling_las("oppna_data"), dbplyr::in_schema("polisen", "brott_niva_nyckel")) %>%
   collect()
 
 # läs in från vår geodatabas i postgis/postgres
-kommun_sf <- hamta_karta("kommuner", regionkoder = "20") %>% 
+# kommun_sf <- hamta_karta("kommuner", regionkoder = "20") %>%
+#   rename(kommunkod = knkod, kommunnamn = knnamn) %>%
+#   st_transform(crs = 4326)
+
+kommun_sf <- tbl(shiny_uppkoppling_las("geodata"), dbplyr::in_schema("karta", "kommun_scb")) %>%                # använd dbplyr för att hämta delar av tabellen
+  filter(str_sub(knkod, 1, 2) == "20") %>%                                               # filtrera ut Dalarnas kommuner
+  collect() %>%                                                                          # först här görs uttaget ur databasen
+  df_till_sf() %>%
   rename(kommunkod = knkod, kommunnamn = knnamn) %>%
   st_transform(crs = 4326)
 
 # läs in från vår geodatabas i postgis/postgres
-deso_sf <- hamta_karta("deso", regionkoder = "20") %>% 
+# deso_sf <- hamta_karta("deso", regionkoder = "20") %>%
+#   rename(kommunkod = kommunkod, kommunnamn = kommunnamn) %>%
+#   st_transform(crs = 4326)
+
+deso_sf <- tbl(shiny_uppkoppling_las("geodata"), dbplyr::in_schema("karta", "deso")) %>%
+  filter(lanskod == "20") %>%                                               # filtrera ut Dalarnas kommuner
+  collect() %>%                                                                          # först här görs uttaget ur databasen
+  df_till_sf() %>%
   rename(kommunkod = kommunkod, kommunnamn = kommunnamn) %>%
   st_transform(crs = 4326)
 
-# läs in från vår postgres-databas som heter oppna_data - där vi skapat denna särskilt för att vi imputerar år som inte finns 
+# läs in från vår postgres-databas som heter oppna_data - där vi skapat denna särskilt för att vi imputerar år som inte finns
 # hos scb på deso och befolking men som finns i polisens data
-bef_df <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("brottsforebyggande", "bef_deso_med_imputering")) %>%
+bef_df <- tbl(shiny_uppkoppling_las("oppna_data"), dbplyr::in_schema("brottsforebyggande", "bef_deso_med_imputering")) %>%
   collect()
 
 # läs in från vår postgres-databas som heter oppna_data
-bra_kommunindikatorer <- tbl(uppkoppling_db("oppna_data"), dbplyr::in_schema("bra", "kommunindikatorer")) %>%
-  collect() %>% 
+bra_kommunindikatorer <- tbl(shiny_uppkoppling_las("oppna_data"), dbplyr::in_schema("bra", "kommunindikatorer")) %>%
+  collect() %>%
   filter(variabel != "Antal svarande i NTU.")
 
 # färgvektor
